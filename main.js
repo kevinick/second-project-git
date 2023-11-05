4560740c873252e4061c3127d00510d195bda949
Kevin Marcelo Lopez Tordoya <kevin.inck@gmail.com>
2023-11-05 19:35:24 -0400

    functionality reset number

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
 }
+function reset() {
+  clicks = 0;
+  document.getElementById("clicks").innerHTML = clicks;
+}
 function multi() {
   clicks *= 2;
   document.getElementById("clicks").innerHTML = clicks;
